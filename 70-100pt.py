5098295a121a7735477458c8efba3937f80c27eb
nickhernandez420 <s571282@student.roundrockisd.org>
2014-10-21 13:58:56 -0500

    working on 70pt

--- 70-100pt.py
+++ 70-100pt.py
@@ -1,28 +1,14 @@
-##########################################
-#                                        #
-#             Draw a house!              #
-#                                        #
-##########################################
-
-# Use create_line(), create_rectangle() and create_oval() to make a 
-# drawing of a house using the tKinter Canvas widget.
-
-# 70pt: House outline (roof and the house)
-# 80pt: Square windows and a door
-# 90pt: A door handle plus a chimney!
-# 100pt: Green grass on the ground and a red house!
-
-# Minus 5pts if your code has no comments
-# Minus 10pts if you only commit once to github
-
 from Tkinter import *
 root = Tk()
 
 # Create the canvas widget
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
+# walls for da house
+rectangle = drawpad.create_rectangle(100,200,400,500)
 
-# Insert your code here to draw the house!
-
-
+#roof
+line1 = drawpad.create_line(100,200,250,40)
+line2 = drawpad.create_line(400,200,250,40)
 root.mainloop()
+
